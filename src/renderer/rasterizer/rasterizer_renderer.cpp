51d22eb1e7a94132ffbd708d911ec13a0fab01e9
exemplerie <exemplerie@mail.ru>
2022-06-02 18:15:28 +0300

    commit_7

--- src/renderer/rasterizer/rasterizer_renderer.cpp
+++ src/renderer/rasterizer/rasterizer_renderer.cpp
@@ -8,10 +8,12 @@ void cg::renderer::rasterization_renderer::init()
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 			settings->width, settings->height);
 	rasterizer = std::make_shared<
-	        cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
+			cg::renderer::rasterizer<cg::vertex, cg::unsigned_color>>();
 	rasterizer->set_viewport(settings->width, settings->height);
 	rasterizer->set_render_target(render_target);
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
 	// TODO: Lab 1.04. Setup an instance of camera `cg::world::camera` class in `cg::renderer::rasterization_renderer`
 	// TODO: Lab 1.06. Add depth buffer in cg::renderer::rasterization_renderer
 }
@@ -19,10 +21,18 @@ void cg::renderer::rasterization_renderer::render()
 {
 	rasterizer->clear_render_target({111, 15, 112});
 
-	cg::utils::save_resource(*render_target, settings->result_path);
-	// TODO: Lab 1.03. Adjust `cg::renderer::rasterization_renderer` class to consume `cg::world::model`
+	for (size_t shape_id = 0; shape_id < model->get_index_buffers().size();
+		 shape_id++)
+	{
+		rasterizer->set_vertex_buffer(model->get_vertex_buffers()[shape_id]);
+		rasterizer->set_index_buffer(model->get_index_buffers()[shape_id]);
+		rasterizer->draw(
+				model->get_index_buffers()[shape_id]->get_number_of_elements(),
+				0);
+	}
 	// TODO: Lab 1.04. Implement `vertex_shader` lambda for the instance of `cg::renderer::rasterizer`
 	// TODO: Lab 1.05. Implement `pixel_shader` lambda for the instance of `cg::renderer::rasterizer`
+	cg::utils::save_resource(*render_target, settings->result_path);
 }
 
 void cg::renderer::rasterization_renderer::destroy() {}
